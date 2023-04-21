4cce0524bf97236e2c4303bdb240671ae321bdf8
Fot28 <polymenakosuk@gmail.com>
2023-04-21 15:05:31 +0300

    Add questions and answers

---
+++ assets/js/questions.js
@@ -0,0 +1,59 @@
+var questionsArray = [
+  {
+    question:
+      "The _______ attribute in HTML is used to specify the URL of the page that the hyperlink goes to.",
+    answers: [
+      ["src", false],
+      ["href", true],
+      ["alt", false],
+      ["title", false],
+    ],
+  },
+  {
+    question:
+      "In CSS, the _______ property is used to add space between the border and the content of ",
+    answers: [["margin"], ["display"], ["border-style"], ["padding", true]],
+  },
+  {
+    question:
+      "The _______ statement in JavaScript is used to stop the execution of a function or a loop.",
+    answers: [["return"], ["continue"], ["break", true], ["throw"]],
+  },
+  {
+    question: "In HTML, the _______ tag is used to define a heading.",
+    answers: [["<p>"], ["<h1>", true], ["<div>"], ["<span>"]],
+  },
+  {
+    question: "The _______ property in CSS is used to set the color of text.",
+    answers: [
+      ["font-weight"],
+      ["text-transform"],
+      ["line-height"],
+      ["color", true],
+    ],
+  },
+  {
+    question:
+      "In JavaScript, the _______ method is used to add an element to the end of an array.",
+    answers: [["push()", true], ["pop()"], ["shift()"], ["unshift()"]],
+  },
+  {
+    question: "The _______ tag in HTML is used to define a paragraph.",
+    answers: [["<p>", true], ["<div>"], ["<span>"], ["<article>"]],
+  },
+  {
+    question:
+      "In CSS, the _______ property is used to set the height of an element.",
+    answers: [["width"], ["position"], ["height", true], ["float"]],
+  },
+  {
+    question:
+      "The _______ function in JavaScript is used to select an element from the DOM.",
+    answers: [
+      ["Math.random()"],
+      ["console.log()"],
+      ["typeof()"],
+      ["document.querySelector()", true],
+    ],
+  },
+];
